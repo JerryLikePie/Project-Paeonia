89dff56add153c8ea2ebc815f9982a3b1b4b670b
ggloria <Gloria_234@163.com>
2022-03-17 13:35:02 +0800

    旧版本备份

---
+++ Assets/Scripts/Utils/UserConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+	public class UserConfig : MonoBehaviour
+	{
+
+		private const string configFileName = "user.dat";
+
+		private WWW www;
+
+		// Use this for initialization
+		void Start()
+		{
+			www = new WWW(Application.streamingAssetsPath + "/" + configFileName);
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
