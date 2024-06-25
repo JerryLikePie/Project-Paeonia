19421dfe791d75523ca99565c94ada7fe822bc04
JerryXie03 <xie389@purdue.edu>
2024-06-25 18:19:07 -0400

    added spine

--- Assets/Scripts/Story/LetterPlay.cs
+++ Assets/Scripts/Story/LetterPlay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ public class LetterPlay : MonoBehaviour
     Canvas letterPrefab;
     public AudioSource flipSound;
     public Text words;
+    public Text title;
     public Image photo;
 
     private void Start()
@@ -16,17 +18,27 @@ public class LetterPlay : MonoBehaviour
         letterPrefab.enabled = false;
     }
 
-    public void ShowLetter(string addWord, Sprite addPhoto)
+    public void ShowLetter(string name)
     {
-        words.text = addWord;
-        photo.sprite = addPhoto;
+        if (string.IsNullOrEmpty(name))
+        {
+            return;
+        }
+        Letterscene scene = (Letterscene)Resources.Load("Stories/" + name);
+        flipSound.Play();
+        words.text = scene.words;
+        title.text = scene.title;
+        photo.sprite = scene.photo;
         letterPrefab.enabled = true;
+        PlayerPrefs.SetInt(name, 4);
     }
 
     public void HideLetter()
     {
         words.text = "placeholder";
+        title.text = "placeholder";
+        flipSound.Play();
         photo.sprite = null;
-        letterPrefab.enabled = true;
+        letterPrefab.enabled = false;
     }
 }
