504d3a497968541f3cf0e328903fd4a9d9b50248
JerryXie03 <xie389@purdue.edu>
2024-11-22 21:54:22 -0500

    Added new Textures

---
+++ Assets/4K Tiled Ground Textures p1/Scripts/AutoSpriteChanger_gp1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class AutoSpriteChanger_gp1 : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+    private int currentSpriteIndex = 0;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (sprites.Length == 0)
+        {
+            Debug.LogWarning("No sprites assigned. Add sprites to the array in the inspector.");
+            return;
+        }
+
+        StartCoroutine(ChangeSpriteEverySecond());
+    }
+
+    private IEnumerator ChangeSpriteEverySecond()
+    {
+        while (true)
+        {
+            spriteRenderer.sprite = sprites[currentSpriteIndex];
+            currentSpriteIndex = (currentSpriteIndex + 1) % sprites.Length;
+            yield return new WaitForSeconds(0.7f);
+        }
+    }
+}
